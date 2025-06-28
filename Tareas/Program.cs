db3a200acb0fd8c46cc08adf97f12189d535704f
Salumelian1 <salustianomeliann@gmail.com>
2025-06-28 12:10:32 -0300

    Se terminos el ejercicio 1

---
+++ Tareas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using Tareas;
+using System.Text.Json;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        string url = "https://jsonplaceholder.typicode.com/todos/";
+        HttpResponseMessage response = await client.GetAsync(url);
+        string ResposeBody = await response.Content.ReadAsStringAsync();
+        List<Tarea> listTarea = JsonSerializer.Deserialize<List<Tarea>>(ResposeBody)!;
+        List<Tarea> TareasC = new List<Tarea>();
+        foreach (var Tarea in listTarea)
+        {
+            Console.WriteLine($"Título: {Tarea.title} | Completada: {Tarea.completed}");
+            if (Tarea.completed)
+            {
+                TareasC.Add(Tarea);
+            }
+        }
+        string JsonCompletas = JsonSerializer.Serialize(TareasC,new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText("completadas.json", JsonCompletas);
+    }
+}
---
+++ Tareas/Tareas.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Tareas
+{
+    public class Tarea
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string? title { get; set; }
+        public bool completed{ get; set; }
+    }
+}
